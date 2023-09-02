3c9d9575a6d34e2bc429a88b43fc7e50e91a8036
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2023-09-02 22:37:58 +0500

    Create modern.js

---
+++ modern.js
@@ -0,0 +1,20 @@
+// This function take an array that contain 3 valuse first and second arr.
+function valueChecker(a,b)  {
+
+    let elements1 = ['A','H','K']; 
+    let elements2 = ['B','I',"L"];
+
+    let indexToShow = 0;
+
+    if(elements1[0] > elements1[1]){   // Its show that which value is greater like 'A'
+
+        console.log(indexToShow = 0);
+    }
+    else if (elements1[1] > elements1[0]) {
+        
+        return elements1[indexToShow]
+    } 
+  }
+ let func = valueChecker(2+2)
+
+    console.log(func); // print this function  in terminal window.
