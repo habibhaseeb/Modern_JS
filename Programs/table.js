fc77c74194cd0c415b76b50cdfec341ec83cc959
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2023-11-30 19:32:49 +0500

    Add files via upload

---
+++ Programs/table.js
@@ -0,0 +1,11 @@
+// let we make a program to print table in console of any numbers using switch statement
+function table(){
+    let L = [1,2,3,4,5,6,7,8,9,10]
+    let L2 = L.map((number1 =>{return number1 * 10})); // this line takes that which value is used to
+            return L2; 
+            //  print the table like table of 2 or anyone
+// so the number 10 in function is the table to print if we print other
+// table like 12,15 or anyvalue you write that value in the function.
+}
+
+console.log(table());
